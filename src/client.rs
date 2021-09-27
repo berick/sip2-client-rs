c6162e2d74dd12348470150d6834be725bd5cf94
Bill Erickson <berickxx@gmail.com>
2021-09-27 18:00:30 -0400

    client; message
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use std::str;
 use std::io::prelude::*;
 use std::net::{Shutdown, TcpStream};
 use log::{info, error};
@@ -5,6 +6,8 @@ use super::{Message, Field, FixedField};
 use super::error::Error;
 use super::spec;
 use super::util;
+
+const READ_BUFSIZE: usize = 256;
 
 pub struct Client {
     sip_host: String, // E.g. 127.0.0.1:6001
@@ -42,7 +45,7 @@ impl Client {
         }
     }
 
-    pub fn send(&mut self, msg: Message) -> Result<(), Error> {
+    pub fn send(&mut self, msg: &Message) -> Result<(), Error> {
         let msg_sip = msg.to_sip();
 
         match self.tcp_stream.write(&msg_sip.as_bytes()) {
@@ -55,8 +58,37 @@ impl Client {
     }
 
     pub fn recv(&mut self) -> Result<Message, Error> {
-        // TODO
-        Message::from_sip("")
+
+        let mut text = String::from("");
+
+        loop {
+
+            let mut buf: [u8; READ_BUFSIZE] = [0; READ_BUFSIZE];
+
+            let num_bytes = match self.tcp_stream.read(&mut buf) {
+                Ok(num) => num,
+                Err(s) => {
+                    error!("recv() failed: {}", s);
+                    return Err(Error::NetworkError);
+                }
+            };
+
+            if num_bytes == 0 { break; }
+
+            let chunk = match str::from_utf8(&buf) {
+                Ok(s) => s,
+                Err(s) => {
+                    error!("recv() got non-utf data: {}", s);
+                    return Err(Error::MessageFormatError);
+                }
+            };
+
+            text.push_str(chunk);
+
+            if num_bytes < READ_BUFSIZE { break; }
+        }
+
+        Message::from_sip(&text)
     }
 }
 
